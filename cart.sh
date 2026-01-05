2f2ba6a50e07bb167e1f56d4a27887764c283085
Vivek Talatam <vivektalatam2000@gmail.com>
2026-01-05 16:25:49 +0530

    cart changes

--- cart.sh
+++ cart.sh
@@ -2,14 +2,15 @@ dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 useradd roboshop
+cp cart.service /etc/systemd/system/cart.service
+sed -i -e 's/<REDIS-SERVER-IP>/redis-dev.botparts.shop' -e 's/<CATALOGUE-SERVER-IP>/catalogue-dev.botparts.shop' /etc/systemd/system/cart.service
 mkdir /app
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
 cd /app 
 unzip /tmp/cart.zip
 cd /app 
 npm install 
-cp cart.service /etc/systemd/system/cart.service
-sed -i -e 's/<REDIS-SERVER-IP>/redis-dev.botparts.shop' -e 's/<CATALOGUE-SERVER-IP>/catalogue-dev.botparts.shop' /etc/systemd/system/cart.service
+
 systemctl daemon-reload
 systemctl enable cart
 systemctl start cart
