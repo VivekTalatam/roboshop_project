4d54e40281c4938181b1cf3cad837180e635157e
Vivek Talatam <vivektalatam2000@gmail.com>
2026-01-05 10:14:20 +0530

    updates

--- user.sh
+++ user.sh
@@ -9,7 +9,7 @@ unzip /tmp/user.zip
 cd /app 
 npm install
 cp user.service /etc/systemd/system/user.service
-sed -i -e 's/<REDIS-IP-ADDRESS>/redis-dev.botparts.shop' -e 's/<MONGODB-SERVER-IP-ADDRESS>/mongodb-dev.botparts.shop' /etc/systemd/system/user.service
+sed -i -e 's/<REDIS-IP-ADDRESS>/redis-dev.botparts.shop/' -e 's/<MONGODB-SERVER-IP-ADDRESS>/mongodb-dev.botparts.shop/' /etc/systemd/system/user.service
 systemctl daemon-reload
 systemctl enable user
 systemctl start user
