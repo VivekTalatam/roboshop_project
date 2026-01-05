ae909a5fb2ba8e900e2c3577d1ca5623b2899530
Vivek Talatam <vivektalatam2000@gmail.com>
2026-01-05 12:22:03 +0530

    chnages catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -2,14 +2,14 @@ dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 useradd roboshop
+cp catalogue.service /etc/systemd/system/catalogue.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 mkdir /app 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
 cd /app 
 unzip /tmp/catalogue.zip
 cd /app 
 npm install
-cp catalogue.service /etc/systemd/system/catalogue.service
-cp mongo.repo /etc/yum.repos.d/mongo.repo
 sed -i 's/<MONGODB-SERVER-IPADDRESS>/mongodb.dev.botparts.shop/g' /etc/systemd/system/catalogue.service
 dnf install mongodb-mongosh -y
 mongosh --host mongodb-dev.botparts.shop </app/db/master-data.js
