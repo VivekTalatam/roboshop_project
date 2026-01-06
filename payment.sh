47a9190ee553436dfbf10d599b42e45b2628f803
Vivek Talatam <vivektalatam2000@gmail.com>
2026-01-06 17:29:46 +0530

    payment changes

--- payment.sh
+++ payment.sh
@@ -1,5 +1,6 @@
 dnf install python3 gcc python3-devel -y
 useradd roboshop
+cp payment.service /etc/systemd/system/payment.service
 mkdir /app 
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip 
 cd /app 
